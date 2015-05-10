ea17b3434c0ea4e2889c940a7848d1f0e1f36e2e
xjflyttp <xjflyttp@gmail.com>
2015-05-10 17:47:47 +0800

    update

--- EasingAsset.php
+++ EasingAsset.php
@@ -8,7 +8,7 @@ class EasingAsset extends AssetBundle {
 
     public $sourcePath = '@bower/jquery.easing';
     public $basePath = '@webroot/assets';
-    public $js = ['jquery.easing.js'];
+    public $js = ['js/jquery.easing.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
 }
